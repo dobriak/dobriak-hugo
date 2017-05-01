e89da9652da1a5f349eb2605e08a36f3fe93784e
Julian Neytchev <dobriak@gmail.com>
2017-05-01 01:46:29 -0700

    rebuilding site Mon May  1 01:46:29 MST 2017

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,19 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo --theme=hugo-theme-pixyll
 
-# Go To Public folder
-cd public
+rm -rf ../dobriak.github.io
+cp -r public/* ../dobriak.github.io
+pushd ../dobriak.github.io
 # Add changes to git.
 git add -A
-
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
 # Push source and build repos.
 git push origin master
-
 # Come Back
-cd ..
+popd
 echo "Done"
